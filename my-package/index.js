3dde62dc31867146c07c613104eea167a2bf5a24
Syed Abdul Samad <abdul.samad>
2020-12-01 16:25:26 +0200

    CRUD operations practice with consent and simple static data source.

---
+++ my-package/index.js
@@ -0,0 +1,2 @@
+module.exports.add = (a, b) => {return a + b;}
+module.exports.multiply = (a, b) => {return a * b;} 
---
+++ use-my-package/index.js
@@ -0,0 +1,3 @@
+const myPackage = require("my-package1231");
+const result = myPackage.add(2,3);
+console.log( result + myPackage.multiply(2,3));
