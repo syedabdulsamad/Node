2aa7f8f90596c01661872e84881fc486fc257031
Syed Abdul Samad <abdul.samad>
2020-12-05 16:52:45 +0200

    Used promises

--- express-practice/asyncpractice.js
+++ express-practice/asyncpractice.js
@@ -1,48 +1,69 @@
 console.log("First");
 
-getUser(1, showUser);
 
-function showUser(user) {
-    console.log(`User is :${JSON.stringify(user)}`);
-    getGitHubRepos(user.name, displayReposAndFindCommits);
+
+// function showUser(user) {
+//     console.log(`User is :${JSON.stringify(user)}`);
+//     getGitHubRepos(user.name, displayReposAndFindCommits);
+// }
+
+// function displayReposAndFindCommits(repos) {
+//     console.log(`Repos are ${JSON.stringify(repos)}`);
+//     getCommits(repos, displayCommits)
+// }
+
+
+
+getUser(1)
+    .then(user => {
+        console.log("User : ", user)
+        return getGitHubRepos(user.userName)
+    })
+    .then(repos => {
+        console.log("Repos : ", JSON.stringify(repos))
+        return getCommits(repos)
+    })
+    .then(commits => displayCommits(commits));
+
+
+function displayCommits(commits) {
+    console.log("Commits: ", commits);
 }
 
-function displayReposAndFindCommits(repos) {
-    console.log(`Repos are ${JSON.stringify(repos)}`);
-    getCommits(repos, displayCommits)
+function getUser(id) {
+    console.log("Getting user");
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                "id": id,
+                "name": "sam"
+            })
+        }, 2000);
+    })
 }
 
-function displayCommits() {
-    console.log("Commits");
+function getGitHubRepos(userName) {
+    console.log("Getting repos for user");
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(["Node", "NodeJs", "Angular"]);
+        }, 2000);
+    })
 }
 
-function getUser(id, callback) {
-    setTimeout(() => {
-        callback({
-            "id": id,
-            "name": "sam"
-        })
-    }, 2000);
+function getCommits(repos) {
+    console.log("Getting commits for repos");
 
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([
+                "id",
+                "name",
+                "sam"
+            ])
+        }, 2000);
+    })
 }
-
-function getGitHubRepos(userName, callback) {
-    setTimeout(() => {
-        callback(["Node", "NodeJs", "Angular"]);
-    }, 2000);
-}
-
-function getCommits(repos, callback) {
-    setTimeout(() => {
-        callback([
-            "id",
-            "name",
-            "sam"
-        ])
-    }, 2000);
-
-}
-
-
-
 console.log("Second");
